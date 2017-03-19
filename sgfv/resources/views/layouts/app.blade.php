9821680b9804ab1ab1c310f39c29448dd306f0f8
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 19:57:05 -0300

    Add - [reservas]: criando item 'cadastro' no menu de template

--- sgfv/resources/views/layouts/app.blade.php
+++ sgfv/resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reservas <span class="caret"></span></a>
                             <ul class="dropdown-menu">
+                            <li><a href="{{ route('reservas.viewCadastrar') }}">Cadastrar</a></li>
                                 <li><a href="{{ route('reservas.consultar') }}">Consultar</a></li>                                
                             </ul>
                         </li>
