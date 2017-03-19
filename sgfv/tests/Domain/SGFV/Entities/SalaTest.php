36d89ea0df7cde8e381f4b6247dea8ad2a573231
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 08:51:51 -0300

    Add - [teste unitário]: testAoCadastrarSalaSeNaoInformarNomeEntaoDeveFalhar

--- sgfv/tests/Domain/SGFV/Entities/SalaTest.php
+++ sgfv/tests/Domain/SGFV/Entities/SalaTest.php
@@ -45,9 +45,36 @@ class SalaTest extends TestCase
         $validator = \Validator::make($request,$rules, $messages);
         $errors = $validator->errors();
 
-
         $this->assertTrue( $validator->fails() );
         $this->assertTrue( $errors->has('codigo') );
         $this->assertEquals( 'O campo código é de preenchimento obrigatório', $errors->first('codigo') );
+    }
+
+    public function testAoCadastrarSalaSeNaoInformarNomeEntaoDeveFalhar()
+    {
+        $request = [
+            'codigo' => 'ABC01',
+            'localizacao' => 'Setor ABC',
+            'created_by' => 1
+        ];
+
+        //
+
+        $rules = [
+            'nome' => 'required'
+        ];
+
+        //
+
+        $messages = [
+            'nome.required' => 'O campo nome é de preenchimento obrigatório'
+        ];
+
+        $validator = \Validator::make($request,$rules, $messages);
+        $errors = $validator->errors();
+
+        $this->assertTrue( $validator->fails() );
+        $this->assertTrue( $errors->has('nome') );
+        $this->assertEquals( 'O campo nome é de preenchimento obrigatório', $errors->first('nome') );
     }
 }
