bff4b213245dd0e82246c4bd841f4954e5a99e61
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 16:26:48 -0300

    Add - [base model]: 'observer' para atribuir valores ao salvar uma entidade.

--- sgfv/Domain/SGFV/Entities/BaseModel.php
+++ sgfv/Domain/SGFV/Entities/BaseModel.php
@@ -6,8 +6,28 @@ class BaseModel extends Model
 {
     protected $dates = ['created_at', 'updated_at'];
 
+    // Relations
+
     public function createdBy()
     {
         return $this->belongsTo( User::class,'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo( User::class,'updated_by');
+    }
+
+    // Observer
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::updating(function( $model ){
+            
+            $model->updated_at = date('Y-m-d H:i:s');
+            $model->updated_by = \Auth::user()->id;
+        });
     }
 }
