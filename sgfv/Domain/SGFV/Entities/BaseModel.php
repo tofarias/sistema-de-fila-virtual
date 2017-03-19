91e0f708bc650115559bb7829e450b83ed399ab6
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 14:45:14 -0300

    Add - [base model]: relacionamento com 'User'

--- sgfv/Domain/SGFV/Entities/BaseModel.php
+++ sgfv/Domain/SGFV/Entities/BaseModel.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class BaseModel extends Model
 {
     protected $dates = ['created_at', 'updated_at'];
+
+    public function createdBy()
+    {
+        return $this->belongsTo( User::class,'created_by');
+    }
 }
