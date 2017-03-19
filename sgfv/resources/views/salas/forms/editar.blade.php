993bfbf8258c3e1ae1657dbf0c06ef178cc2c539
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 16:57:04 -0300

    Add - [salas]: definindo tamanho de campos

--- sgfv/resources/views/salas/forms/editar.blade.php
+++ sgfv/resources/views/salas/forms/editar.blade.php
@@ -2,19 +2,19 @@
     <div class="col-md-4">
         <div class="form-group">
             {{ Form::label('Código')}}
-            {{ Form::text('codigo', null, ['class' => 'form-control', 'autofocus']) }}
+            {{ Form::text('codigo', null, ['class' => 'form-control', 'autofocus', 'maxlength' => 5]) }}
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
             {{ Form::label('Nome')}}
-            {{ Form::text('nome', null, ['class' => 'form-control']) }}
+            {{ Form::text('nome', null, ['class' => 'form-control', 'maxlength' => 20]) }}
         </div>
     </div>
     <div class="col-md-4">
         <div class="form-group">
             {{ Form::label('Localização')}}
-            {{ Form::text('localizacao', null, ['class' => 'form-control']) }}
+            {{ Form::text('localizacao', null, ['class' => 'form-control', 'maxlength' => 50]) }}
         </div>
     </div>
 </div>
