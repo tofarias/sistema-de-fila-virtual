e1f465cad26d25a1845935682a2b01096ac53c29
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 19:47:27 -0300

    Add - [reserva]: criando relacionamento com a entidade 'Sala'

--- sgfv/Domain/SGFV/Entities/ReservaSala.php
+++ sgfv/Domain/SGFV/Entities/ReservaSala.php
@@ -5,4 +5,11 @@ class ReservaSala extends BaseModel
     protected $primaryKey =  'reserva_id';
 
     protected $dates = ['dt_inicio', 'dt_fim'];
+
+    //
+
+    public function sala()
+    {
+        return $this->belongsTo( Sala::class, 'sala_id' );
+    }
 }
