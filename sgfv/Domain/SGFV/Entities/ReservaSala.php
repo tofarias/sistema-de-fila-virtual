cf3357e086dc27598fa498b2864c6d678d55ce44
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 23:51:00 -0300

    Add - [reservas]: observer

--- sgfv/Domain/SGFV/Entities/ReservaSala.php
+++ sgfv/Domain/SGFV/Entities/ReservaSala.php
@@ -12,4 +12,32 @@ class ReservaSala extends BaseModel
     {
         return $this->belongsTo( Sala::class, 'sala_id' );
     }
+
+    // Observers
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function( $model ){
+            
+            $model->created_at = date('Y-m-d H:i:s');
+            $model->created_by = \Auth::user()->id;
+
+            $total = self::where(function($query) use ($model) {
+                            $query->where('dt_fim', '>=', $model->dt_inicio)
+                                  ->where('dt_fim', '<=', $model->dt_fim);
+                        })->where( 'sala_id', '=', $model->sala_id)
+                          ->orWhere(function( $query ) use($model){
+                              $query->where('created_by', '=', $model->created_by)
+                                    ->where('sala_id', '!=', $model->sala_id);
+                          })
+                          ->count();
+
+            if( $total > 0 )
+            {
+                throw new \DomainException('Já existe reserva para este horário');
+            }
+        });
+    }
 }
