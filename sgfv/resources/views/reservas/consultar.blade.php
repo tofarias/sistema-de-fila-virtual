041205e7ba4a99c5bae4883e57d46c06cb9a822a
Tiago Farias <tiago.farias.poa@gmail.com>
2017-03-19 23:53:34 -0300

    Add - [reservas]: exibindo mensagem em casso de erro

--- sgfv/resources/views/reservas/consultar.blade.php
+++ sgfv/resources/views/reservas/consultar.blade.php
@@ -5,6 +5,11 @@
 <div class="container">
 
     <div class="row">
+    @if (session('fail'))
+        <div class="alert alert-warning">
+            {{ session('fail') }}
+        </div>
+    @endif
         <div class="col-md-12">
             <div class="well">
                 {{ Form::open(['route' => 'reservas.consultar', 'method' => 'GET', 'class' => ""]) }}
